5bb74c8016951b9dc6bdb41da7149caa23dc81f0
Yu Shi <yushiys95@gmail.com>
2021-01-16 02:24:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["wow", "British", "Nebelung", "Persian"]
+  array = ["wow", "British", "Nebelung"]
   array[1]
 end
 
