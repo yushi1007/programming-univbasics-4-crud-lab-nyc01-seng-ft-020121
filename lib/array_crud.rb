8949b1885597cd84f2e183db0f4240330c70e230
Yu Shi <yushiys95@gmail.com>
2021-01-16 02:21:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  famous_cats = ["Exotic Shorthair", "British Shorthair", "Nebelung", "Persian cat"]
+  famous_cats = ["Shorthair", "British", "Nebelung", "Persian"]
   famous_cats[2]
 end
 
