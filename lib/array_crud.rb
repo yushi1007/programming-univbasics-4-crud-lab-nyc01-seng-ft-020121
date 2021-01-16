baf71a0dcbc281ee74366d60b5c56ac8de96c11e
Yu Shi <yushiys95@gmail.com>
2021-01-16 01:53:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -43,5 +43,5 @@ end
 
 def update_element_from_index(array, index_number, element)
   famous_cats = ["Exotic Shorthair", "British Shorthair", "Nebelung", "Persian cat"]
-  famous_cats[3] = 
+  famous_cats[3] = "nice cat"
 end
