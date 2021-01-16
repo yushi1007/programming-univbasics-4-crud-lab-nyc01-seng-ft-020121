481f17d56ac31ce8a1bf4bd35b22b081ba302596
Yu Shi <yushiys95@gmail.com>
2021-01-16 02:25:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,15 +31,13 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array[]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  famous_cats = ["Exotic Shorthair", "British Shorthair", "Nebelung", "Persian cat"]
   famous_cats[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-  famous_cats = ["Exotic Shorthair", "British Shorthair", "Nebelung", "Persian cat"]
-  famous_cats[3] = "nice cat"
+  famous_cats[3] 
 end
