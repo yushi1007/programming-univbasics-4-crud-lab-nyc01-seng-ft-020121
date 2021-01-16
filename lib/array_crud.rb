12cce2e713b4f13bf0a141638f43a17ec9f05a6c
Yu Shi <yushiys95@gmail.com>
2021-01-16 01:46:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
  famous_cats = ["Exotic Shorthair", "British Shorthair", "Nebelung", "Persian cat"]
- cats.push ("arrays!")
+ famous_cats.push ("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
