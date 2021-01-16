8f33abb0d1853e6fc5db46347f94cc7249c13c6f
Yu Shi <yushiys95@gmail.com>
2021-01-16 02:19:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["cat fish", "British Shorthair", "Nebelung", "Persian cat"]
+  array = ["cat", "Shorthair", "Nebelung", "Persian"]
   array.shift
 end
 
