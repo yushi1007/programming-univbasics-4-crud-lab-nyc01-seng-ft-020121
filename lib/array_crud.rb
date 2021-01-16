7f39262d19d7c04bbf5d8403748de6109e59db1f
Yu Shi <yushiys95@gmail.com>
2021-01-16 02:02:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array = ["Shorthair", "British", "Nebelung", "Persian"]
+  array = ["Shorthair", "British", "Nebelung", "Persian",  "nice"]
   array.unshift ("fish")
 end
 
