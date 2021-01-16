3fc2df68dc40e5d7aa4510f22832b71d4b52a07a
Yu Shi <yushiys95@gmail.com>
2021-01-16 02:22:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   cats = ["Shorthair", "British", "Nebelung", "Persian"]
-  cats[2]
+  cats[1]
 end
 
 def retrieve_first_element_from_array(array)
