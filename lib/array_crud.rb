c44943ad6ad67a322e953eeb594acb73cd8b5787
Yu Shi <yushiys95@gmail.com>
2021-01-16 02:08:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-array = ["hi", "add", "me", "to", "front"]
-array.unshift("wow")
+array = ["hi", "nice", "to", "meet", "you"]
+array.unshift("cool")
 end
 
 def remove_element_from_end_of_array(array)
