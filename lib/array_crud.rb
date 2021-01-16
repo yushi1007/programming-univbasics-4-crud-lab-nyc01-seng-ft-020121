cd535d3bbfb9b6ef3dba0441de09ecc12b2c48c7
Yu Shi <yushiys95@gmail.com>
2021-01-16 01:49:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift()
+  famous_cats = ["cat fish", "British Shorthair", "Nebelung", "Persian cat"]
+  cat fish = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
