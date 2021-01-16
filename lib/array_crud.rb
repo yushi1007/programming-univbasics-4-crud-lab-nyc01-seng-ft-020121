aa66edf85a18a560b025bdb666abedb47c0d4cb8
Yu Shi <yushiys95@gmail.com>
2021-01-16 02:27:58 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,23 +7,27 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
- array << element
+ famous_cats = ["Exotic Shorthair", "British Shorthair", "Nebelung", "Persian cat"]
+ famous_cats.push ("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift(element)
+  array = ["Shorthair", "British", "Nebelung", "Persian",  "nice"]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop()
+  array = ["Exotic Shorthair", "British Shorthair", "Nebelung", "arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift()
+  array = ["wow", "Shorthair", "Nebelung", "Persian"]
+  wow = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[index_number]
+ array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
@@ -35,5 +39,7 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-array[index_number] = element
+    array = ["I", "am", "yu", "shi"]
+    array[1]
+    array[1] = "totally"
 end
