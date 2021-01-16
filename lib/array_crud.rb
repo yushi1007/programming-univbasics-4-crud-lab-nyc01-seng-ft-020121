2f72c038457da7bb702aba32839dc236fed555cd
Yu Shi <yushiys95@gmail.com>
2021-01-16 01:50:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   famous_cats = ["Exotic Shorthair", "British Shorthair", "Nebelung", "Persian cat"]
-  array[2]
+  famous_cats[2]
 end
 
 def retrieve_first_element_from_array(array)
